9513f668688d0cb62450510a2631416856c6d491
Amanshriffle15 <amank@shriffle.com>
2023-08-22 18:48:37 +0530

    add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_052752) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_071622) do
   create_table "carts", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "product_variant_id", null: false
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_052752) do
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_carts_on_customer_id"
     t.index ["product_variant_id"], name: "index_carts_on_product_variant_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.integer "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -56,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_052752) do
 
   add_foreign_key "carts", "customers"
   add_foreign_key "carts", "product_variants"
+  add_foreign_key "comments", "products"
   add_foreign_key "product_variants", "products"
   add_foreign_key "products", "vendors"
 end
